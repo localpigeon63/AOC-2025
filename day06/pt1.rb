b3547ff98fb11d3f86f8e4e6d8db2ce779b8365b
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2025-12-06 09:36:59 +0000

    day6 pt1 mapped out but not coded

---
+++ day06/pt1.rb
@@ -0,0 +1,5 @@
+#check number of rows in input - this gives length of problem
+# initialise matching number of arrays and store in a master array
+# turn each row of input into an array of characters
+# iterate through and for each character, by index, push into corresponding array in master array
+# for each array, check if contains * or +, then delete final element and multiply or add all other elements together
