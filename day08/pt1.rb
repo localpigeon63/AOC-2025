de782f5c74c7412745d2396e181719522dcb78a2
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2025-12-13 22:50:44 +0000

    Day08pt1 done

--- day08/pt1.rb
+++ day08/pt1.rb
@@ -1,12 +1,75 @@
-#generate all combinations and straight-line numbers (shrinking list each time to avoid duplicates, e.g. 2*30 and 30*2)
-#(this is very inefficient, we'll see if I get away with it)
-#what is a straight-line number and how to calculate? see saved chat
-#store each pair of coordinates and the corresponding straight-line number in a hash
-#order the hash by straight-line numbers (looks simple from google)
-#iterate through the first 1000 entries
-#for each, check whether any of the coordinates is stored in an array - it will be inside a sub-array, so something like arr.any?.include?
-#if so, push coordinates to that array
-#if not, initialise new array and store those coordinates in there (in a sub-array, so they still count as 1 item)
-#after 1000 iterations done
-#order overall array by sub-array length
-#multiply together length of three largest items
+input= File.readlines("./inputs/day08.txt")
+
+Vector = Data.define(:x, :y, :z)
+
+def find_distance(a, b)
+    x2 = b.x - a.x
+    y2 = b.y - a.y
+    z2 = b.z - a.z
+    Math.sqrt(x2 ** 2 + y2 ** 2 + z2 ** 2)
+end
+
+vectors = input.map do |line|
+    a, b, c = line.chomp.split(",").map {|num| num.to_i}
+    Vector.new(a,b,c)
+end
+
+distances = {}
+
+# all but last
+vectors[..-2].each_with_index do |vector1, index|
+    # from the next one until the end
+    vectors[index+1..].each do |vector2|
+        distances[[vector1, vector2]] = find_distance(vector1, vector2)
+    end
+end
+
+sorted_distances = distances.sort_by { |_, value| value }.to_h
+
+circuits = []
+
+sorted_distances.first(1000).each do |boxes, distance|
+    box_0_circuit = nil
+    box_1_circuit = nil
+    circuits.each_with_index do |circuit, index|
+        if circuit.include?(boxes[0])
+            box_0_circuit = index
+        end
+        if circuit.include?(boxes[1])
+            box_1_circuit = index
+        end
+    end
+
+    box_0_disconnected = box_0_circuit.nil?
+    box_1_disconnected = box_1_circuit.nil?
+    box_0_has_circuit = !box_0_circuit.nil?
+    box_1_has_circuit = !box_1_circuit.nil?
+
+    if box_0_disconnected && box_1_disconnected
+        circuits << [boxes[0], boxes[1]]
+    elsif box_0_has_circuit && box_1_disconnected
+        circuits[box_0_circuit] << boxes[1]
+    elsif box_0_disconnected && box_1_has_circuit
+        circuits[box_1_circuit] << boxes[0]
+    elsif box_0_circuit == box_1_circuit && box_0_has_circuit
+        next
+    elsif box_0_has_circuit && box_1_has_circuit && box_0_circuit != box_1_circuit
+        merge(box_0_circuit, box_1_circuit, circuits)
+    end
+end
+
+def merge(index1, index2, circuits)
+     combined_circuit = circuits[index1] + circuits[index2]
+    [index1, index2].sort.reverse.each do |index|
+            circuits.delete_at(index)
+    end
+    circuits << combined_circuit
+end
+
+circuits.sort_by!{ |circuit| circuit.length }.reverse!
+
+puts "Part one answer: #{circuits[0].length * circuits[1].length * circuits[2].length}"
+
+
+
+
